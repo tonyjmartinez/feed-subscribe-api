a170bae6387f58685c7233c223e70245e089f08c
Tony Martinez <ajosephmartinez@gmail.com>
2020-02-24 14:06:53 -0800

    .

--- get.js
+++ get.js
@@ -3,23 +3,16 @@ import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
   const params = {
-    TableName: process.env.tableName,
-    // 'Key' defines the partition key and sort key of the item to be retrieved
-    // - 'userId': Identity Pool identity id of the authenticated user
-    // - 'noteId': path parameter
-    Key: {
-      userId: event.requestContext.identity.cognitoIdentityId,
-      commentId: event.pathParameters.id
-    }
+    TableName: process.env.tableName
   };
 
   try {
-    const result = await dynamoDbLib.call("get", params);
-    if (result.Item) {
+    const result = await dynamoDbLib.call("query", params);
+    if (result.Items) {
       // Return the retrieved item
-      return success(result.Item);
+      return success(result.Items);
     } else {
-      return failure({ status: false, error: "Item not found." });
+      return failure({ status: false, error: "Failed to fetch comments." });
     }
   } catch (e) {
     console.log(e);
--- getComments.js
+++ getComments.js
@@ -1,16 +1,46 @@
 const { ApolloServer, gql } = require("apollo-server-lambda");
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
     hello: String
   }
+  type Comments {
+    userId: String
+    commentId: String
+  }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => "Hello world!"
+    hello: () => "Hello world!",
+    comments: async () => {
+      const params = {
+        TableName: process.env.graphQLTable,
+        // 'Key' defines the partition key and sort key of the item to be retrieved
+        // - 'userId': Identity Pool identity id of the authenticated user
+        // - 'noteId': path parameter
+        Key: {
+          userId: event.requestContext.identity.cognitoIdentityId,
+          commentId: event.pathParameters.id
+        }
+      };
+      try {
+        const result = await dynamoDbLib.call("get", params);
+        if (result.Item) {
+          // Return the retrieved item
+          return success(result.Item);
+        } else {
+          return failure({ status: false, error: "Item not found." });
+        }
+      } catch (e) {
+        console.log(e);
+        return failure({ status: false });
+      }
+    }
   }
 };
 
